f56cbc1435052e40842e89e0d1eed6c7de82dd34
Daniel Popperl <danielpopperl@hotmail.com>
2023-10-13 22:14:29 -0300

    ok

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -17,6 +17,12 @@ export default async function SignIn() {
           allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;"
         ></iframe>
       </div>
+
+      <div>
+        <video width="320" height="240" id="myVideo2">
+          <source src="https://iframe.mediadelivery.net/play/161168/3692e87b-e322-4bb5-bb8a-fb93f72523c8"></source>
+        </video>
+      </div>
     </>
   );
 }
